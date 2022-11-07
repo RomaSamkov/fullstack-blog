a57d7a21fea47326141d3e7392074a1862f83f05
RomaSamkov <Romansamkov.rs@gmail.com>
2022-11-07 23:16:17 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import * as dotenv from "dotenv";
 dotenv.config();
+import authRouter from "./routes/auth.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Success" });
-});
-
-const { DB_HOST } = process.env;
+const DB_HOST = process.env.DB_HOST;
 const PORT = process.env.PORT || 3030;
+
+app.use("/api/auth", authRouter);
 
 const start = async () => {
   try {
     await mongoose.connect(DB_HOST);
+    // .then(() => console.log("DB OK"))
+    // .catch((error) => console.log("DB Error", error));
     app.listen(PORT, () => console.log(`Server sterted on Port:${PORT}`));
   } catch (error) {
     console.log(error);
