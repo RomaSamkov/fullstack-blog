e216afb2efb1edfeceb43eabf910612f5215d350
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-01 22:55:07 +0200

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getCurrent, login, register } from "../controllers/auth.js";
+import { checkAuth } from "../utils/checkAuth.js";
 
 const router = new Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/current", getCurrent);
+router.get("/current", checkAuth, getCurrent);
 
 export default router;
