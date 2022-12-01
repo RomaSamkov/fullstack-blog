bf41c6786b238f94ea695ef45c490737f4cb6743
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-01 22:54:59 +0200

    Update checkAuth.js

--- server/utils/checkAuth.js
+++ server/utils/checkAuth.js
@@ -2,4 +2,28 @@ import jwt from "jsonwebtoken";
 
 export const checkAuth = (req, res, next) => {
   const token = (req.headers.Authorization || "").replace(/Bearer\s?/, "");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.userId = decoded.id;
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.json({ message: "No Access" });
+    }
+  } else {
+    console.log(error);
+    return res.json({ message: "No Access" });
+  }
+  // try {
+  //   if (!token) {
+  //     return res.json({ message: "No Access token" });
+  //   }
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //   req.userId = decoded.id;
+  //   next();
+  // } catch (error) {
+  //   console.log(error);
+  //   res.json({ message: "No Access" });
+  // }
 };
