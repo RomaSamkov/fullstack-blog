6f693da4d1c4110a3c6bb12897458a81d8fc89b6
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-02 23:18:18 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { AddPostPage } from "./pages/AddPostPage";
+import { EditPostPage } from "./pages/EditPostPage";
+import { LoginPage } from "./pages/LoginPage";
+import { MainPage } from "./pages/MainPage";
+import { PostPage } from "./pages/PostPage";
+import { PostsPage } from "./pages/PostsPage";
+import { RegisterPage } from "./pages/RegisterPage";
+
 function App() {
-  return <div className="text-2xl text-red-600">FullStack Blog</div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="posts" element={<PostsPage />} />
+        <Route path=":id" element={<PostPage />} />
+        <Route path=":id/edit" element={<EditPostPage />} />
+        <Route path="new" element={<AddPostPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
